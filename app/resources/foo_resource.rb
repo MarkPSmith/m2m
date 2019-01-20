e39dd8fd8321decc84d2680cb81dec6b9397d3ff
Mark Smith <mark@estimatesoftware.com>
2019-01-20 14:51:24 -0500

    Get rid of do section on many_to_many :children

--- app/resources/foo_resource.rb
+++ app/resources/foo_resource.rb
@@ -1,14 +1,5 @@
 class FooResource < ApplicationResource
   attribute :name, :string
-  many_to_many :children, resource: FooResource, foreign_key: { children: :child_foo_id }, m2m_where_foreign_key: :foo_id do
-    assign do |parents, children|
-      parents.each do |parent|
-        parent.association(:children).loaded!
-        children.each do |child|
-          parent.children << child
-        end
-      end
-    end
-  end
-  many_to_many :parents, resource: FooResource, m2m_where_foreign_key: :foo_id
+  many_to_many :children, resource: FooResource, foreign_key: { children: :child_foo_id }, through_primary_key: :foo_id
+  many_to_many :parents, resource: FooResource, through_primary_key: :foo_id
 end
