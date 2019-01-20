f31e0d28c275fdf744ee0d137b9bff1ee1940f69
Mark Smith <mark@estimatesoftware.com>
2019-01-20 14:51:34 -0500

    Add index specs

--- spec/api/v1/foos/index_spec.rb
+++ spec/api/v1/foos/index_spec.rb
@@ -19,4 +19,29 @@ RSpec.describe "foos#index", type: :request do
       expect(d.map(&:id)).to match_array([foo1.id, foo2.id])
     end
   end
+
+  describe 'm2m fetch' do
+    let!(:foo1) {create (:foo)}
+    let!(:foo2) {create (:foo)}
+    let!(:foo3) {create (:foo)}
+
+    it 'fetches the children of foo1' do
+      foo1.children << foo2 << foo3
+      jsonapi_get '/api/v1/foos?include=children'
+      expect(jsonapi_data[0].relationships["children"]["data"].length).to eq(2)
+      expect(jsonapi_included.length).to eq(2)
+    end
+
+    it 'fetches no children for foo2' do
+      foo1.children << foo2 << foo3
+      jsonapi_get '/api/v1/foos?include=children'
+      expect(jsonapi_data[1].relationships["children"]["data"].length).to eq(0)
+    end
+
+    it 'fetches no children for foo3' do
+      foo1.children << foo2 << foo3
+      jsonapi_get '/api/v1/foos?include=children'
+      expect(jsonapi_data[2].relationships["children"]["data"].length).to eq(0)
+    end
+  end
 end
